5ad48dc3c44a8d6efb428e3adfef18648b7b1658
sazzad ul alam Shawon <93247296+sazzadulalambd@users.noreply.github.com>
2022-09-16 23:53:11 +0600

    CV

---
+++ script.js
@@ -0,0 +1,12 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab")
+}
